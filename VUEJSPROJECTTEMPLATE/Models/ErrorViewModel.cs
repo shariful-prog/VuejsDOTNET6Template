5399854ec6511cc75e54beb48dd90fadca93560f
shariful.islam <shariful.islam@numberskills.se>
2023-06-15 17:13:54 +0600

    added

---
+++ VUEJSPROJECTTEMPLATE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VUEJSPROJECTTEMPLATE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
